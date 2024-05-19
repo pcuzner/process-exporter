cc625bd65f275782ebe04172bdb3eddf41168ad0
Paul Cuzner <pcuzner@ibm.com>
2024-05-20 11:29:41 +1200

    build: bump to alpine 3.19
    Signed-off-by: Paul Cuzner <pcuzner@ibm.com>

--- buildah/build-exporter.sh
+++ buildah/build-exporter.sh
@@ -12,15 +12,15 @@ echo "Build image with the tag: $TAG"
 
 # podman pull the image first, push to your local registry to speed up local builds
 #IMAGE="docker.io/golang:1.19"
-IMAGE="laptop:5000/golang:1.19"
+IMAGE="localhost:5000/golang:1.19"
 build=$(buildah from $IMAGE)
 buildah add $build ../src /process-exporter
 buildah config --workingdir /process-exporter $build
 buildah run -e GOOS=linux -e GOARCH=amd64 -e CGO_ENABLED=0 -- $build go build .
 
-# as above, grab the alpine image first and push to a local registry
-#container=$(buildah from "docker.io/alpine:3.17")
-container=$(buildah from "laptop:5000/alpine:3.17")
+# as above, grab the alpine image first and push to a local registry (was 3.17)
+#container=$(buildah from "docker.io/alpine:3.19")
+container=$(buildah from "localhost:5000/alpine:3.19")
 buildah config --workingdir / $container
 buildah copy --from $build $container /process-exporter/process-exporter /process-exporter
 buildah config --entrypoint '["/process-exporter"]' $container
